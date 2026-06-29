1ab6f51d9d9a9eeaa4278fdd8e67075d116d5908
Eliana Assefa <166456293+elianaAbay@users.noreply.github.com>
2026-06-29 17:48:32 -0400

    Update: minimum-stack - submission-0

--- Data Structures & Algorithms/minimum-stack/submission-0.py
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -2,22 +2,26 @@ class MinStack:
 
     def __init__(self):
         self.stack = []
-        self.minstack = []
+        self.minVal = 0
         
 
     def push(self, val: int) -> None:
         self.stack.append(val)
-        val = min(val, self.minstack[-1] if self.minstack else val)
-        self.minstack.append(val)
+        
 
     def pop(self) -> None:
         self.stack.pop()
-        self.minstack.pop()
-        
 
     def top(self) -> int:
         return self.stack[-1]
         
     def getMin(self) -> int:
-        return self.minstack[-1]
+        print(self.stack)
+        minimum = float("inf")
+        for item in self.stack:
+            minimum = min(minimum, item)
+        return minimum
+
+
+        
         
