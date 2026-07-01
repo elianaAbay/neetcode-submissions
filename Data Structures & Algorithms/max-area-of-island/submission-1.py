db5c4f6c744095b9b496976e313a46306c5b34de
Eliana Assefa <166456293+elianaAbay@users.noreply.github.com>
2026-07-01 19:08:43 -0400

    Update: max-area-of-island - submission-1

--- Data Structures & Algorithms/max-area-of-island/submission-1.py
+++ Data Structures & Algorithms/max-area-of-island/submission-1.py
@@ -1,34 +1,23 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        row , col = len(grid), len(grid[0])
+        row, col = len(grid), len(grid[0])
+        maxCount = 0 
+        visited = set()
 
-
-        found = set()
         def backtrack(r,c):
 
-            if r < 0 or c < 0 or r >= row or c >= col or grid[r][c] == 0 or ((r,c)) in found:
+            if r < 0 or c < 0 or r >= row or c >= col or (r,c) in visited or grid[r][c] == 0:
                 return 0
-
-            found.add((r,c)) 
-            return 1 + backtrack(r + 1, c) + backtrack(r - 1, c) + backtrack(r, c+ 1) + backtrack(r, c- 1)
-
-
-
-        island = 0 
+            visited.add((r,c))
+            return (1 + backtrack(r-1,c) + backtrack(r+1, c) + backtrack(r, c - 1)+ backtrack(r, c + 1))
+        
+        count = 0
         for r in range(row):
-            for c in range(col):
-                if grid[r][c] == 1 and ((r,c)) not in found: 
-                    island = max(island, backtrack(r, c))
-
-        return island 
-                    
             
-
+            for c in range(col):
+                if grid[r][c] == 1 and (r,c) not in visited:
+                    maxCount = max(backtrack(r,c), maxCount)
         
-
-
-
-        
-
+        return maxCount
 
 
