8a7b428a3d2dc43581ee8b4fe8d46f905f6d2f45
Eliana Assefa <166456293+elianaAbay@users.noreply.github.com>
2026-07-01 18:26:28 -0400

    Update: count-number-of-islands - submission-1

--- Data Structures & Algorithms/count-number-of-islands/submission-1.py
+++ Data Structures & Algorithms/count-number-of-islands/submission-1.py
@@ -1,29 +1,27 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-        ROWS, COLS = len(grid), len(grid[0])
-        islands = 0
+        if not grid:
+            return 0 
+        row, col = len(grid), len(grid[0])
+        visited = set()
+        numIsland = 0
 
-        def bfs(r, c):
-            q = deque()
-            grid[r][c] = "0"
-            q.append((r, c))
+        def isIsland(r,c):
+            if r < 0 or c < 0 or  r >= row or c >= col or (r,c) in visited or grid[r][c] == "0": 
+                return 
 
-            while q:
-                row, col = q.popleft()
-                for dr, dc in directions:
-                    nr, nc = dr + row, dc + col
-                    if (nr < 0 or nc < 0 or nr >= ROWS or
-                        nc >= COLS or grid[nr][nc] == "0"
-                    ):
-                        continue
-                    q.append((nr, nc))
-                    grid[nr][nc] = "0"
+            visited.add((r,c))
+            isIsland(r + 1, c)
+            isIsland(r - 1, c)
+            isIsland(r, c + 1)
+            isIsland(r, c - 1)
 
-        for r in range(ROWS):
-            for c in range(COLS):
-                if grid[r][c] == "1":
-                    bfs(r, c)
-                    islands += 1
 
-        return islands
+        for r in range(row):
+            for c in range(col):
+                if grid[r][c] == "1" and (r,c) not in visited:
+                    isIsland(r, c)
+                    numIsland += 1
+                
+        return numIsland 
+
