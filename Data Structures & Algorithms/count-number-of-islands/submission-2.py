1fdb778f5434a03c6c9b0667575cf44a16882c12
Eliana Assefa <166456293+elianaAbay@users.noreply.github.com>
2026-07-01 18:26:52 -0400

    Update: count-number-of-islands - submission-2

--- Data Structures & Algorithms/count-number-of-islands/submission-2.py
+++ Data Structures & Algorithms/count-number-of-islands/submission-2.py
@@ -1,35 +1,26 @@
-from typing import List
-
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
         
-        rows, cols = len(grid), len(grid[0])
+        row, col = len(grid), len(grid[0])
         visited = set()
+        numIsland = 0
 
-        def dfs(r, c):
-           
-            if (
-                r < 0 or c < 0 or 
-                r >= rows or c >= cols or 
-                grid[r][c] == "0" or 
-                (r, c) in visited
-            ):
-                return
-            
-            visited.add((r, c))
-            
-            dfs(r + 1, c)
-            dfs(r - 1, c)
-            dfs(r, c + 1)
-            dfs(r, c - 1)
+        def isIsland(r,c):
+            if r < 0 or c < 0 or  r >= row or c >= col or (r,c) in visited or grid[r][c] == "0": 
+                return 
 
-        islands = 0
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == "1" and (r, c) not in visited:
-                    dfs(r, c)
-                    islands += 1
+            visited.add((r,c))
+            isIsland(r + 1, c)
+            isIsland(r - 1, c)
+            isIsland(r, c + 1)
+            isIsland(r, c - 1)
 
-        return islands
+
+        for r in range(row):
+            for c in range(col):
+                if grid[r][c] == "1" and (r,c) not in visited:
+                    isIsland(r, c)
+                    numIsland += 1
+                
+        return numIsland 
+
