6526e66c1886cef0907cc65d9eadb8ff3ce340a4
Eliana Assefa <166456293+elianaAbay@users.noreply.github.com>
2026-07-02 17:24:36 -0400

    Update: subsets - submission-1

--- Data Structures & Algorithms/subsets/submission-1.py
+++ Data Structures & Algorithms/subsets/submission-1.py
@@ -1,28 +1,17 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        #so out list should not contain any duplicated. 
-        #-> [], [[]]
-        #using a set, out memory complecity ->o(n), n being the subset we've created 
-        # [], [1], [1,2], [1,3], [2], [2, 3], [3], [1,2,3], time complecity o(n^2)
+        subset =[]
+        perm = []
 
-        res = []
-        subset = []
-        def dfs(i):
-            if i >= len(nums):
-                res.append(subset.copy())
+        def backtrack(i):
+            if i == len(nums):
+                subset.append(perm.copy())
                 return 
-
             
-            #the decision to include num[i]
-            subset.append(nums[i])
-            dfs(i+1)
-
-            #decision not to include nums[i]
-            subset.pop()
-            dfs(i + 1)
-
-        dfs(0)
-        return res 
-
-
-            
+            perm.append(nums[i])
+            backtrack(i + 1)
+            perm.pop()
+            backtrack(i+1)
+        
+        backtrack(0)
+        return subset
