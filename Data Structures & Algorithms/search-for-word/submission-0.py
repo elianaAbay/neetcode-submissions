a262ac747cb602033c6b7934a63dc674e07c6097
Eliana Assefa <166456293+elianaAbay@users.noreply.github.com>
2026-07-04 13:59:08 -0400

    Update: search-for-word - submission-0

--- Data Structures & Algorithms/search-for-word/submission-0.py
+++ Data Structures & Algorithms/search-for-word/submission-0.py
@@ -1,28 +1,25 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        ROWS, COLS = len(board), len(board[0])
-        path = set()
+        row, col = len(board), len(board[0])
+        visited = set()
 
-        def dfs(r, c, i):
+
+        def backtrack(r,c,i):
             if i == len(word):
-                return True
+                return True 
+            if r < 0 or c < 0 or r > row - 1 or c > col - 1 or (r,c) in visited or board[r][c] != word[i]:
+                return False 
 
-            if (min(r, c) < 0 or
-                r >= ROWS or c >= COLS or
-                word[i] != board[r][c] or
-                (r, c) in path):
-                return False
+            visited.add((r,c))
 
-            path.add((r, c))
-            res = (dfs(r + 1, c, i + 1) or
-                   dfs(r - 1, c, i + 1) or
-                   dfs(r, c + 1, i + 1) or
-                   dfs(r, c - 1, i + 1))
-            path.remove((r, c))
+            res = (backtrack(r + 1,c,i + 1) or backtrack(r -1,c,i + 1) or backtrack(r,c - 1,i + 1) or backtrack(r,c + 1,i + 1))
+            visited.remove((r,c))
+            
             return res
 
-        for r in range(ROWS):
-            for c in range(COLS):
-                if dfs(r, c, 0):
+        for r in range(row):
+            for c in range(col):
+                if backtrack(r,c,0):
                     return True
+        
         return False
