488a0b738c1dd4c3b2a262e775550a9f1098bce4
Eliana Assefa <166456293+elianaAbay@users.noreply.github.com>
2026-07-25 15:12:22 -0400

    Update: binary-tree-right-side-view - submission-0

--- Data Structures & Algorithms/binary-tree-right-side-view/submission-0.py
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-0.py
@@ -7,25 +7,24 @@
 
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        
         res = []
-        q = collections.deque([root])
-  
 
+        q = deque([root])
 
         while q:
-            right = None 
+            rightSide = None 
             qLen = len(q)
 
             for i in range(qLen):
                 node = q.popleft()
 
                 if node:
-                    right = node
-                    q.append(right.left)
-                    q.append(right.right)
-
-            if right:
-                res.append(right.val)       
-
+                    rightSide = node 
+                    q.append(node.left)
+                    q.append(node.right)
+                
+            
+            if rightSide:
+                res.append(rightSide.val)
+            
         return res
